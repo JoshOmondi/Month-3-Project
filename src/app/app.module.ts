1f90045d7b2c51209b48d95731761b3a74714c99
Joshua Omondi Ochieng <joshuaomondi3334#gmail.com>
2023-12-05 12:06:02 +0300

    added the horm, sigh up and sign in components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ProfileComponent } from './profile/profile.component';
+import { CommentsComponent } from './comments/comments.component';
+import { UpdateUserComponent } from './update-user/update-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent,
+    CommentsComponent,
+    UpdateUserComponent
   ],
   imports: [
     BrowserModule,
