9f8ecdde2b4ad39079d70759e8b03e0f7365ad68
Joshua Omondi Ochieng <joshuaomondi3334#gmail.com>
2023-12-11 16:18:08 +0300

    fix:made some changes on the navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
+  isDropdownVisible = false;
 
+  toggleDropdown(event: Event): void {
+    event.preventDefault(); 
+    this.isDropdownVisible = !this.isDropdownVisible;
+  }
 }
